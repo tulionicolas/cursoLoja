dffc87eb2f666f02a73a831ae71561860a8159fa
Tulio Nicolas Nogueira de Oliveira <tulionicolas@outlook.com>
2016-04-13 15:35:52 -0300

    Arquivo Loja

---
+++ TestGitHub/Controllers/LojaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestGitHub.Controllers
+{
+    public class LojaController : Controller
+    {
+        // GET: Loja
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
